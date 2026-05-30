70117411575f6a00c29026a8a007a6ceb0d42711
noel0519 <noeldavid586@gmail.com>
2026-05-30 18:58:34 +0530

    Add files via upload

---
+++ makefile
@@ -0,0 +1,14 @@
+CC=arm-none-eabi-gcc
+MACH=cortex-m4
+CFLAGS= -c -mcpu=$(MACH) -mthumb -std=gnu11 -o0
+
+all:main.o stm32_startup.o
+
+main.o:main.c
+	$(CC) $(CFLAGS)  -o $@ $^
+	
+stm32_startup.o:stm32_startup.c
+	$(CC) $(CFLAGS)  -o $@ $^
+	
+clean:
+	rm -rf *.o* *.elf
---
+++ stm32_startup.c
@@ -0,0 +1,23 @@
+#include<stdint.h>
+
+#define SRAM_START  0x20000000U
+#define SRAM_SIZE (128*1024)
+#define SRAM_END (SRAM_START) + (SRAM_SIZE)
+
+
+#define STACK_START SRAM_END
+
+void Reset_handler(void);
+
+
+uint32_t vectors[] = {
+	STACK_START,
+	(uint32_t)&Reset_handler,
+	
+};
+
+
+void Reset_handler(void){
+	
+	
+}
